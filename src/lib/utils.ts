1715dffee19e9e7be8b280e08103b3256499237e
Yuto Ueki <ueki.yuto.bz@gmail.com>
2026-02-24 05:00:17 +0900

    test: lint error検証用

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,6 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+const unused = "lint error"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
